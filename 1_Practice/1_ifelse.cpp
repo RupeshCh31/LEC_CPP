b9569f427ccfeee4cc0c2f7e89ebbcad880fbec8
RupeshCh31 <rupesh.ch855@gmail.com>
2023-05-23 01:02:38 -0700

    first chapter

--- 1_Practice/1_ifelse.cpp
+++ 1_Practice/1_ifelse.cpp
@@ -5,5 +5,21 @@ int main()
     int number;
     cout<<"Enter any number:"<<endl;
     cin>>number;
+    if (number!=0)
+    {
+        if (number>0)
+        {
+        cout<<"The number is positive"<<endl;
+        }
+        else
+        {
+            cout<<"The number is negative"<<endl;
+        }
+
+    }
+    else
+    {
+        cout<<"The number is zero"<<endl;
+    }
     return 0;
 }
