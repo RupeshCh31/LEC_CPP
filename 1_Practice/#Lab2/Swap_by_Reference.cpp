33d9259f6739dc4cae1100a5313463f01d3a673f
RupeshCh31 <rupesh.ch855@gmail.com>
2023-05-31 00:23:12 -0700

    lab

--- 1_Practice/#Lab2/Swap_by_Reference.cpp
+++ 1_Practice/#Lab2/Swap_by_Reference.cpp
@@ -6,8 +6,8 @@ int main()
 {
     int num1 = getnum();
     int num2 = getnum();
-    cout<<"Values before swap :"<<"num1"<<"num2";
+    cout<<"Values before swap :"<<num1<<","<<num2;
     swap(num1 , num2);
-    cout<<"Values after swap :"<<"num1"<<"num2";
+    cout<<"Values after swap :"<<num1<<","<<num2;
     return 0;
 }
